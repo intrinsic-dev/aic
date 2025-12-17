9e02200719b1b709f78e50f846f02c6b1bab7b96
Carlos Agüero <caguero@osrfoundation.org>
2025-12-17 18:00:00 +0100

    Style
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>

--- aic_gazebo/src/ScoringPlugin.cc
+++ aic_gazebo/src/ScoringPlugin.cc
@@ -16,12 +16,13 @@
  *
  */
 
-#include "proto/scoring.pb.h"
 #include "aic_gazebo/ScoringPlugin.hh"
 
 #include <gz/common/Console.hh>
-#include <gz/sim/Conversions.hh>
 #include <gz/plugin/Register.hh>
+#include <gz/sim/Conversions.hh>
+
+#include "proto/scoring.pb.h"
 
 GZ_ADD_PLUGIN(aic_gazebo::ScoringPlugin, gz::sim::System,
               aic_gazebo::ScoringPlugin::ISystemConfigure,
@@ -30,11 +31,10 @@ GZ_ADD_PLUGIN(aic_gazebo::ScoringPlugin, gz::sim::System,
 
 namespace aic_gazebo {
 //////////////////////////////////////////////////
-void ScoringPlugin::Configure(
-    const gz::sim::Entity& _entity,
-    const std::shared_ptr<const sdf::Element>& _sdf,
-    gz::sim::EntityComponentManager& /*_ecm*/,
-    gz::sim::EventManager& /*_eventManager*/) {
+void ScoringPlugin::Configure(const gz::sim::Entity& _entity,
+                              const std::shared_ptr<const sdf::Element>& _sdf,
+                              gz::sim::EntityComponentManager& /*_ecm*/,
+                              gz::sim::EventManager& /*_eventManager*/) {
   gzdbg << "aic_gazebo::ScoringPlugin::Configure on entity: " << _entity
         << std::endl;
 
@@ -42,12 +42,11 @@ void ScoringPlugin::Configure(
   double rate = _sdf->Get<double>("update_rate", 1).first;
   std::chrono::duration<double> period{rate > 0 ? 1 / rate : 0};
   this->updatePeriod =
-    std::chrono::duration_cast<std::chrono::steady_clock::duration>(period);
+      std::chrono::duration_cast<std::chrono::steady_clock::duration>(period);
 
   this->topic = _sdf->Get<std::string>("topic", "/aic/gazebo/data").first;
   this->pub = this->node.Advertise<msgs::Scoring>(this->topic);
-  if (!pub)
-  {
+  if (!pub) {
     std::cerr << "Error advertising topic [" << this->topic << "]" << std::endl;
     return;
   }
@@ -59,17 +58,15 @@ void ScoringPlugin::PreUpdate(const gz::sim::UpdateInfo& _info,
   // Throttle update rate.
   auto elapsed = _info.simTime - this->lastUpdateTime;
   if (elapsed > std::chrono::steady_clock::duration::zero() &&
-      elapsed < this->updatePeriod)
-  {
+      elapsed < this->updatePeriod) {
     return;
   }
   this->lastUpdateTime = _info.simTime;
 
   this->scoringMsg.mutable_header()->mutable_stamp()->CopyFrom(
-    gz::sim::convert<gz::msgs::Time>(_info.simTime));
+      gz::sim::convert<gz::msgs::Time>(_info.simTime));
 
-  if (!this->pub.Publish(this->scoringMsg))
-    return;
+  if (!this->pub.Publish(this->scoringMsg)) return;
 }
 
 //////////////////////////////////////////////////
