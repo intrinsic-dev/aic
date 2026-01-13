6c836595ffed0e3264cab7de17621d5f3bb7aa2d
Carlos Agüero <caguero@osrfoundation.org>
2026-01-13 18:59:36 +0100

    Tweaks
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>

--- aic_gazebo/src/OffLimitContactsPlugin.hh
+++ aic_gazebo/src/OffLimitContactsPlugin.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Intrinsic Innovation LLC
+ * Copyright (C) 2026 Intrinsic Innovation LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
