c6ec3aceaf44084618f9a191527dcbc378c2a9e5
Carlos Agüero <caguero@osrfoundation.org>
2026-01-13 10:17:15 +0100

    Always check collision with the arm
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>

--- aic_gazebo/src/OffLimitContactsPlugin.cc
+++ aic_gazebo/src/OffLimitContactsPlugin.cc
@@ -76,13 +76,18 @@ void OffLimitContactsPlugin::PreUpdate(const UpdateInfo &_info,
           const components::ContactSensorData *_contacts) -> bool {
         // Only consider contacts with off-limit models (e.g. enclosure).
         for (const auto &contact : _contacts->Data().contact()) {
-          auto found1 = this->offLimitEntities.find(
-                            topLevelModel(contact.collision1().id(), _ecm)) !=
-                        this->offLimitEntities.end();
-          auto found2 = this->offLimitEntities.find(
-                            topLevelModel(contact.collision2().id(), _ecm)) !=
-                        this->offLimitEntities.end();
-          shouldPublish = shouldPublish || found1 || found2;
+          shouldPublish = 
+            ((this->offLimitEntities.find(
+              topLevelModel(contact.collision1().id(), _ecm)) !=
+                this->offLimitEntities.end()) &&
+            (topLevelModel(contact.collision2().id(), _ecm) == 
+              this->modelEntity)) ||
+            ((this->offLimitEntities.find(
+              topLevelModel(contact.collision2().id(), _ecm)) !=
+                this->offLimitEntities.end()) &&
+              (topLevelModel(contact.collision1().id(), _ecm) == 
+                this->modelEntity));
+
           if (shouldPublish) break;
         }
         if (shouldPublish) this->publisher.Publish(_contacts->Data());
