1dcb4792a80d94df06682de725dfe2de0b34d9c3
Morgan Quigley <morganquigley@intrinsic.ai>
2026-01-20 20:17:21 -0500

    style

--- aic_model/test/cycle_through_lifecycle.py
+++ aic_model/test/cycle_through_lifecycle.py
@@ -29,7 +29,9 @@ class CycleLifecycleNode(Node):
     def __init__(self):
         super().__init__("cycle_lifecycle")
         self.get_client = self.create_client(GetState, "aic_model_node/get_state")
-        self.change_client = self.create_client(ChangeState, "aic_model_node/change_state")
+        self.change_client = self.create_client(
+            ChangeState, "aic_model_node/change_state"
+        )
         while not self.get_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("waiting for get_state service...")
         while not self.change_client.wait_for_service(timeout_sec=1.0):
