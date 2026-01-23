017a578577e0883f4b4092bf4beab0deb9b85814
Carlos Aguero <caguero@osrfoundation.org>
2026-01-23 15:40:26 +0000

    Move jerk to a separate branch
    Signed-off-by: Carlos Aguero <caguero@osrfoundation.org>

--- aic_scoring/src/ScoringTier2.cc
+++ aic_scoring/src/ScoringTier2.cc
@@ -17,19 +17,15 @@
 
 #include "aic_scoring/ScoringTier2.hh"
 
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2/LinearMath/Quaternion.h>
 #include <yaml-cpp/yaml.h>
 
 #include <algorithm>
 #include <chrono>
-#include <cmath>
 #include <iostream>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <rosbag2_cpp/writer.hpp>
 #include <string>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <vector>
 
 namespace aic_scoring {
@@ -258,115 +254,4 @@ ScoringTier2Node::ScoringTier2Node(const std::string &_yamlFile)
     return;
   }
 }
-
-//////////////////////////////////////////////////
-bool ScoringTier2::UpdateJerk(const geometry_msgs::msg::PoseStamped &_pose) {
-  // Helper to convert ROS time to seconds.
-  auto toSeconds = [](const builtin_interfaces::msg::Time &t) {
-    return static_cast<double>(t.sec) + static_cast<double>(t.nanosec) * 1e-9;
-  };
-
-  // Check timestamp is increasing.
-  if (!this->poseHistory.empty()) {
-    double lastTime = toSeconds(this->poseHistory.back().header.stamp);
-    double newTime = toSeconds(_pose.header.stamp);
-    if (newTime <= lastTime) {
-      return false;
-    }
-  }
-
-  // Add pose to history.
-  this->poseHistory.push_back(_pose);
-
-  // Need 4 samples to compute jerk.
-  if (this->poseHistory.size() < 4) {
-    return true;
-  }
-
-  // Keep only last 4 samples.
-  if (this->poseHistory.size() > 4) {
-    this->poseHistory.erase(this->poseHistory.begin());
-  }
-
-  // Extract timestamps.
-  double t0 = toSeconds(this->poseHistory[0].header.stamp);
-  double t1 = toSeconds(this->poseHistory[1].header.stamp);
-  double t2 = toSeconds(this->poseHistory[2].header.stamp);
-  double t3 = toSeconds(this->poseHistory[3].header.stamp);
-
-  // Helper to convert quaternion to Euler angles (roll, pitch, yaw).
-  auto quatToEuler = [](const geometry_msgs::msg::Quaternion &q, double &roll,
-                        double &pitch, double &yaw) {
-    tf2::Quaternion quat_tf;
-    tf2::fromMsg(q, quat_tf);
-    tf2::Matrix3x3(quat_tf).getRPY(roll, pitch, yaw);
-  };
-
-  // Extract positions.
-  double px[4], py[4], pz[4];
-  for (int i = 0; i < 4; ++i) {
-    px[i] = this->poseHistory[i].pose.position.x;
-    py[i] = this->poseHistory[i].pose.position.y;
-    pz[i] = this->poseHistory[i].pose.position.z;
-  }
-
-  // Extract Euler angles.
-  double roll[4], pitch[4], yaw[4];
-  for (int i = 0; i < 4; ++i) {
-    quatToEuler(this->poseHistory[i].pose.orientation, roll[i], pitch[i],
-                yaw[i]);
-  }
-
-  // Compute finite differences for jerk.
-  // v1 = (p1 - p0) / (t1 - t0), etc.
-  // a1 = (v2 - v1) / (midpoint difference)
-  // jerk = (a2 - a1) / (midpoint difference)
-  auto computeJerk = [&](const double p[4]) {
-    double v1 = (p[1] - p[0]) / (t1 - t0);
-    double v2 = (p[2] - p[1]) / (t2 - t1);
-    double v3 = (p[3] - p[2]) / (t3 - t2);
-
-    double mid_v1 = (t0 + t1) / 2.0;
-    double mid_v2 = (t1 + t2) / 2.0;
-    double mid_v3 = (t2 + t3) / 2.0;
-
-    double a1 = (v2 - v1) / (mid_v2 - mid_v1);
-    double a2 = (v3 - v2) / (mid_v3 - mid_v2);
-
-    double mid_a1 = (mid_v1 + mid_v2) / 2.0;
-    double mid_a2 = (mid_v2 + mid_v3) / 2.0;
-
-    return (a2 - a1) / (mid_a2 - mid_a1);
-  };
-
-  // Compute linear jerk.
-  this->linearJerk.x = computeJerk(px);
-  this->linearJerk.y = computeJerk(py);
-  this->linearJerk.z = computeJerk(pz);
-
-  // Compute angular jerk.
-  this->angularJerk.x = computeJerk(roll);
-  this->angularJerk.y = computeJerk(pitch);
-  this->angularJerk.z = computeJerk(yaw);
-
-  return true;
-}
-
-//////////////////////////////////////////////////
-geometry_msgs::msg::Vector3 ScoringTier2::GetLinearJerk() const {
-  return this->linearJerk;
-}
-
-//////////////////////////////////////////////////
-geometry_msgs::msg::Vector3 ScoringTier2::GetAngularJerk() const {
-  return this->angularJerk;
-}
-
-//////////////////////////////////////////////////
-void ScoringTier2::ResetJerk() {
-  this->poseHistory.clear();
-  this->linearJerk = geometry_msgs::msg::Vector3();
-  this->angularJerk = geometry_msgs::msg::Vector3();
-}
-
 }  // namespace aic_scoring
