9e184db7fdb24fc055a2faf68916152345e81f4b
Grace Kwak <gkwak@intrinsic.ai>
2026-02-05 12:51:18 -0800

    Apply suggestion from @Yadunund
    Co-authored-by: yadunund <yadunund@intrinsic.ai>

--- aic_utils/aic_teleoperation/scripts/cartesian_keyboard_teleop.py
+++ aic_utils/aic_teleoperation/scripts/cartesian_keyboard_teleop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-#  Copyright (C) 2025 Intrinsic Innovation LLC
+#  Copyright (C) 2026 Intrinsic Innovation LLC
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
