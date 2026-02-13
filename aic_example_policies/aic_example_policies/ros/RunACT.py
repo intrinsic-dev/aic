8cf5ad8a1b158c7bd3ba9dbce8e29ddac2717d5e
Grace Kwak <gkwak@intrinsic.ai>
2026-02-13 15:20:44 -0800

    Resize and normalize observation images

--- aic_example_policies/aic_example_policies/ros/RunACT.py
+++ aic_example_policies/aic_example_policies/ros/RunACT.py
@@ -17,6 +17,7 @@
 import time
 import torch
 import numpy as np
+import cv2
 from typing import Callable, Dict, Any
 from rclpy.node import Node
 from geometry_msgs.msg import Twist, Vector3
@@ -25,49 +26,66 @@ from aic_model.policy import Policy
 from aic_model_interfaces.msg import Observation
 from aic_task_interfaces.msg import Task
 from lerobot.policies.act.modeling_act import ACTPolicy
-
+from safetensors.torch import load_file
 
 class RunACT(Policy):
     def __init__(self, parent_node: Node):
         super().__init__(parent_node)
-
-        policy_path = "/home/aic/ws_aic/outputs/grkw/random_start_poses_10_eps"  # TODO (@grkw): change this to public HF repo once it's released
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # Load Policy
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.policy = ACTPolicy.from_pretrained(policy_path).to(self.device)
+        policy_path = "/home/aic/ws_aic/outputs/grkw/random_start_poses_10_eps"  # TODO (@grkw): change this to public HF repo once it's released
+        self.policy = ACTPolicy.from_pretrained(policy_path).to(self.device) # reads config.json and model.safetensors
         self.policy.eval()
-
         self.get_logger().info(f"ACT Policy loaded on {self.device} from {policy_path}")
 
+        # Image preprocessing
+        stats_path = policy_path + "/policy_preprocessor_step_3_normalizer_processor.safetensors"
+        stats = load_file(stats_path)
+
+        self.left_image_means = stats["observation.images.left_camera.mean"].to("cuda").view(1, 3, 1, 1)
+        self.center_image_means = stats["observation.images.center_camera.mean"].to("cuda").view(1, 3, 1, 1)
+        self.right_image_means = stats["observation.images.right_camera.mean"].to("cuda").view(1, 3, 1, 1)
+        self.get_logger().info(f"Left camera means: {self.left_image_means}")
+
+        self.left_image_stds = stats["observation.images.left_camera.std"].to("cuda").view(1, 3, 1, 1)
+        self.center_image_stds = stats["observation.images.center_camera.std"].to("cuda").view(1, 3, 1, 1)
+        self.right_image_stds = stats["observation.images.right_camera.std"].to("cuda").view(1, 3, 1, 1)
+        self.get_logger().info(f"Left camera stds: {self.left_image_stds}")
+
+        self.image_scaling = 0.25 # manually change to match `AICRobotAICControllerConfig`
+
     @staticmethod
-    def _img_to_tensor(raw_img, device: torch.device) -> torch.Tensor:
+    def _img_to_tensor(raw_img, device: torch.device, image_scale, mean, std) -> torch.Tensor:
         """Helper to convert ROS Image msg to normalized PyTorch tensor."""
         img_np = np.frombuffer(raw_img.data, dtype=np.uint8).reshape(
             raw_img.height, raw_img.width, 3
         )
-        # Permute to (C, H, W) and normalize
-        return (
-            torch.from_numpy(img_np)
-            .permute(2, 0, 1)
-            .float()
-            .div(255.0)
-            .unsqueeze(0)
-            .to(device)
-        )
+        
+        img_np_resized = cv2.resize(
+                        img_np,
+                        None,
+                        fx=image_scale,
+                        fy=image_scale,
+                        interpolation=cv2.INTER_AREA,
+                    )
+        
+        tensor_0_1 = torch.from_numpy(img_np_resized).permute(2, 0, 1).float().div(255.0).unsqueeze(0).to(device)
+        
+        return (tensor_0_1 - mean / std)
 
     def prepare_observations(self, obs_msg: Observation) -> Dict[str, torch.Tensor]:
         """Process camera and controller state for inference, i.e. prepare an observation to send to the policy. The `obs` dict format should match that of `AICRobotAICController` `get_observation()` used for recording episodes."""
         # Process Cameras
         obs = {
             "observation.images.left_camera": self._img_to_tensor(
-                obs_msg.left_image, self.device
+                obs_msg.left_image, self.device, self.image_scaling, self.left_image_means, self.left_image_stds
             ),
             "observation.images.center_camera": self._img_to_tensor(
-                obs_msg.center_image, self.device
+                obs_msg.center_image, self.device, self.image_scaling, self.center_image_means, self.center_image_stds
             ),
             "observation.images.right_camera": self._img_to_tensor(
-                obs_msg.right_image, self.device
+                obs_msg.right_image, self.device, self.image_scaling, self.right_image_means, self.right_image_stds
             ),
         }
 
