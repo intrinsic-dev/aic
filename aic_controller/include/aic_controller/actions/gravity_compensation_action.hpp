ceb1469ae75cb35d9da1c6bc163c34ad09c42504
Trushant Adeshara <trushant@intrinsic.ai>
2026-02-24 17:09:08 -0800

    Remove additional whitespace from comments

--- aic_controller/include/aic_controller/actions/gravity_compensation_action.hpp
+++ aic_controller/include/aic_controller/actions/gravity_compensation_action.hpp
@@ -28,7 +28,7 @@
 
 #include "rclcpp/node_interfaces/node_logging_interface.hpp"
 
-// The versions conditioning is added here to support the 
+// The versions conditioning is added here to support the
 // source-compatibility with ROS 2 Jazzy
 #include "rclcpp/version.h"
 #if RCLCPP_VERSION_GTE(29, 0, 0)
