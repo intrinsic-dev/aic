0b0bfffa3a98192bb98b95d768b2b16073d4a017
Jennifer Buehler <jennifer.e.buehler@gmail.com>
2026-03-16 22:41:01 +0100

    Add expand xacro service

--- aic_bringup/launch/aic_gz_bringup.launch.py
+++ aic_bringup/launch/aic_gz_bringup.launch.py
@@ -222,6 +222,14 @@ def launch_setup(context, *args, **kwargs):
         ],
     )
 
+    xacro_expander = Node(
+        package="aic_bringup",
+        executable="xacro_expander.py",
+        parameters=[
+            {"use_sim_time": True},
+        ],
+    )
+
     aic_engine = Node(
         package="aic_engine",
         executable="aic_engine",
@@ -416,6 +424,7 @@ def launch_setup(context, *args, **kwargs):
         delay_rviz_after_controller_started,
         delay_rviz_after_controller_stopped,
         aic_adapter,
+        xacro_expander,
         gz_ip_env,
         gzserver,
         gzgui,
