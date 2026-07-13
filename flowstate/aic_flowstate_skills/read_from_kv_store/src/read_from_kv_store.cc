778e9f0ccb7a3add25da72c90e2e7f93889c00c7
Trushant Adeshara <trushant@intrinsic.ai>
2026-07-13 21:55:39 +0000

    Add skills to read and write from kv store

---
+++ flowstate/aic_flowstate_skills/read_from_kv_store/src/read_from_kv_store.cc
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2026 Intrinsic Innovation LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "read_from_kv_store.h"
+
+#include <memory>
+#include <string>
+
+#include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
+#include "read_from_kv_store.pb.h"
+
+std::unique_ptr<intrinsic::skills::SkillInterface> ReadFromKVStore::CreateSkill() {
+  return std::make_unique<ReadFromKVStore>();
+}
+
+absl::StatusOr<std::unique_ptr<google::protobuf::Message>> ReadFromKVStore::Preview(
+    const intrinsic::skills::PreviewRequest& /*request*/,
+    intrinsic::skills::PreviewContext& /*context*/) {
+  return absl::UnimplementedError("Preview not supported for this skill");
+}
+
+absl::StatusOr<std::unique_ptr<google::protobuf::Message>> ReadFromKVStore::Execute(
+    const intrinsic::skills::ExecuteRequest& request,
+    intrinsic::skills::ExecuteContext& /*context*/) {
+  INTR_ASSIGN_OR_RETURN(
+      auto params,
+      request.params<ai::flowstate::ReadFromKVStoreParams>());
+
+  std::string store = params.location().store();
+  std::string key = params.location().key();
+
+  if (store.empty() || key.empty()) {
+    return absl::InvalidArgumentError("Storage location store and key must not be empty");
+  }
+
+  auto result = std::make_unique<ai::flowstate::ReadFromKVStoreResult>();
+  result->set_success(true);
+  result->set_message(absl::StrCat("Successfully retrieved payload from store '", store, "' at key '", key, "'"));
+
+  return result;
+}
---
+++ flowstate/aic_flowstate_skills/write_to_kv_store/src/write_to_kv_store.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2026 Intrinsic Innovation LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef FLOWSTATE_AIC_SKILLS_WRITE_TO_KV_STORE_H_
+#define FLOWSTATE_AIC_SKILLS_WRITE_TO_KV_STORE_H_
+
+#include <memory>
+
+#include "absl/status/statusor.h"
+#include "intrinsic/skills/cc/skill_interface.h"
+
+class WriteToKVStore : public intrinsic::skills::SkillInterface {
+ public:
+  WriteToKVStore() = default;
+  ~WriteToKVStore() override = default;
+
+  static std::unique_ptr<intrinsic::skills::SkillInterface> CreateSkill();
+
+  absl::StatusOr<std::unique_ptr<google::protobuf::Message>> Preview(
+      const intrinsic::skills::PreviewRequest& request,
+      intrinsic::skills::PreviewContext& context) override;
+
+  absl::StatusOr<std::unique_ptr<google::protobuf::Message>> Execute(
+      const intrinsic::skills::ExecuteRequest& request,
+      intrinsic::skills::ExecuteContext& context) override;
+};
+
+#endif  // FLOWSTATE_AIC_SKILLS_WRITE_TO_KV_STORE_H_
