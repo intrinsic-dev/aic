dbb71e80c8bdfa753f7faac90b425a4cc88f5b78
Trushant Adeshara <trushant@intrinsic.ai>
2026-07-21 07:44:06 +0000

    Add delete_from_kv_store_skill

---
+++ flowstate/aic_flowstate_skills/delete_from_kv_store/src/delete_from_kv_store.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2026 Intrinsic Innovation LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef AI_FLOWSTATE_SKILLS_DELETE_FROM_KV_STORE_H_
+#define AI_FLOWSTATE_SKILLS_DELETE_FROM_KV_STORE_H_
+
+#include <memory>
+
+#include "absl/status/statusor.h"
+#include "google/protobuf/message.h"
+#include "intrinsic/skills/cc/skill_interface.h"
+
+class DeleteFromKVStore : public intrinsic::skills::SkillInterface {
+ public:
+  DeleteFromKVStore() = default;
+
+  static std::unique_ptr<intrinsic::skills::SkillInterface> CreateSkill();
+
+  absl::StatusOr<std::unique_ptr<google::protobuf::Message>> Preview(
+      const intrinsic::skills::PreviewRequest& request,
+      intrinsic::skills::PreviewContext& context) override;
+
+  absl::StatusOr<std::unique_ptr<google::protobuf::Message>> Execute(
+      const intrinsic::skills::ExecuteRequest& request,
+      intrinsic::skills::ExecuteContext& context) override;
+};
+
+#endif  // AI_FLOWSTATE_SKILLS_DELETE_FROM_KV_STORE_H_
