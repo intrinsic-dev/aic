fdf2337f2653b1d65b3807e9a03e7a2202050559
JohnTGZ <johntangz@google.com>
2026-07-27 22:31:45 +0800

    update testing script

--- flowstate/aic_flowstate_ros_bridge/launch/test_joint_targets.launch.py
+++ flowstate/aic_flowstate_ros_bridge/launch/test_joint_targets.launch.py
@@ -18,21 +18,20 @@ def generate_launch_description():
                         ##########################
                         # Home position:
                         ##########################
-                        "test_joint_targets": "["
-                        "-1.57, -1.57, -1.57, -1.57, 1.57, -1.57"
-                        "]",
+                        # "test_joint_targets": "["
+                        # "-1.57, -1.57, -1.57, -1.57, 1.57, -1.57"
+                        # "]",
                         ##########################
-                        # Multiple positions
+                        # Test 1:
                         ##########################
-                        # 'test_joint_targets': '['
-                        # '-1.57, -1.57, -1.57, -1.57, 1.57, -1.57'
-                        # '-1.57, -1.57, -1.57, -1.57, 1.57, 0.0 '
-                        # ']',
+                        'test_joint_targets': '['
+                        '-1.27, -1.8, -1.47, -1.57, 1.57, -1.57,'
+                        # Home
+                        '-1.57, -1.57, -1.57, -1.57, 1.57, -1.57'
+                        ']',
                         "log_filepath": "",  # Empty string defaults to tcp_accuracy_<DATETIME>.json
-                        "target_pose_stiffness": [1000.0, 150.0],
-                        "target_pose_damping": [200.0, 54.7723],
-                        "target_joint_stiffness": [25.0, 25.0, 25.0, 5.0, 5.0, 5.0],
-                        "target_joint_damping": [5.0, 5.0, 5.0, 5.0, 5.0, 5.0],
+                        "target_joint_stiffness": [100.0, 100.0, 100.0, 25.0, 25.0, 25.0],
+                        "target_joint_damping": [10.0, 10.0, 10.0, 5.0, 5.0, 5.0],
                         "publish_duration": 3.0,
                         "stop_duration": 2.0,
                     }
